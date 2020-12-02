6389f2414870a96dd65c8d585b0aa9d7655f9659
Benjamin Koger <bkoger@gmail.com>
2020-12-01 21:59:04 -0500

    part two done

--- one.cpp
+++ one.cpp
@@ -6,6 +6,37 @@
 
 using namespace std;
 
+bool numInVec(const int &num, const vector<int> &nums) {
+    return (find(nums.begin(), nums.end(), num) != nums.end());
+}
+
+vector<int> find2Num(const int &target, const vector<int> &nums) {
+    vector<int> results;
+    for(auto num : nums) {
+       int num2 = target - num;
+       if(numInVec(num2, nums)) {
+           results.push_back(num);
+           results.push_back(num2);
+           break;
+       }
+   }
+   return results;
+}
+
+vector<int> find3Num(const int &target, const vector<int> &nums) {
+    vector<int> results;
+    for(auto num : nums) {
+        int num1 = num;
+        int left = target - num1;
+        results = find2Num(left, nums);
+        if(results.size() == 2) {
+            results.push_back(num1);
+            break;
+        }
+    }
+    return results;
+}
+
 int main() {
    vector<int> nums;
    const string filename = "aoc2020_input_1.txt";
@@ -18,11 +49,15 @@ int main() {
        }
    }
 
-   for(auto num : nums) {
-       int num2 = 2020 - num;
-       if(find(nums.begin(), nums.end(), num2) != nums.end()) {
-           int product = num * num2;
-           cout << num << " " << num2 << " " << product << "\n";
-       }
+   vector<int> results = find2Num(2020, nums);
+   for(auto n : results) {
+       cout << n << " ";
    }
+   cout <<  results[0] * results[1] <<"\n";
+
+   vector<int> results2 = find3Num(2020, nums);
+   for(auto n : results2) {
+       cout << n << " ";
+   }
+   cout << results2[0] * results2[1] * results2[2] << "\n";
 }
